cbab4e390cbce7ee80eb35cbbe4ee3c27523a898
Emma Labbé <emma@labbe.me>
2026-04-08 14:08:07 -0400

    Support platform customization by individual package

--- README.md
+++ README.md
@@ -17,4 +17,11 @@ $ pyto-pkg --output SwiftPackagePath install numpy
 
 The python version used to run `pyto-pkg` will be the version running `pip`. You can configure multiple Python versions with different packages in a single Swift Package. 
 
+You can pass a requirements file with `-r`. By default, `pyto-pkg` installs wheels for all supported platforms, but you can specify them with the `-t` argument, or you can customize them by package by adding a coma after the package name or version. Then you can write the platforms name, followed by an underscore and the architecture separated by a coma. If you want to just exclude a platform, put an exclamation mark before the platform name. For example:
+
+```
+opencv-contrib-python==4.13.0+d87edc6,!watchos_armv7k
+torch==2.8.0a0+gitde3aca3,ios_arm64,ios-macabi_arm64,ios-macabi_x86_64
+```
+
 Pass `--help` for more options. By default, wheels will be pulled from `https://git.gatit.es/api/packages/pyto/pypi/simple` but you can change it with `--index-url`. Wheels are preferred over tar balls and pip will fallback to the default index repository if no wheels or packages are found in the specified repo index. You can also skip scripts with `--no-scripts` so you can `pip install` the wheels at runtime while keeping the extensions. However you need to be sure that the version of the package installed at runtime matches the version of the extensions stored in the app bundle.
--- pyto_pkg/commands.py
+++ pyto_pkg/commands.py
@@ -2,22 +2,72 @@ from .environment import call_pip, SUPPORTED_TARGETS, DEFAULT_INDEX, OutputPacka
 import os
 
 
+def parse_package_customization(package_spec: str, default_targets: list[str]) -> tuple[str, list[str]]:
+    if "," not in package_spec:
+        return package_spec, default_targets
+    
+    parts = package_spec.split(",")
+    package_name = parts[0]
+    custom_platforms = parts[1:]
+    
+    if not custom_platforms:
+        return package_name, default_targets
+    
+    # Separate includes and excludes
+    includes = []
+    excludes = []
+    
+    for platform_spec in custom_platforms:
+        if platform_spec.startswith("!"):
+            excludes.append(platform_spec[1:])
+        else:
+            includes.append(platform_spec)
+    
+    # Build final target list
+    result_targets = []
+    
+    if includes:
+        # If includes are specified, only use those
+        result_targets = includes
+    else:
+        # If only excludes are specified, use all except excluded ones
+        result_targets = [target for target in default_targets if target not in excludes]
+    
+    return package_name, result_targets
+
+
 def install(output: str, output_target_name: str, packages: list[str] = [], requirement: str = None, no_scripts: bool = False, no_deps: bool = False, index_url: str = DEFAULT_INDEX, targets: list[str] = [], include: list[str] = []):
     package = OutputPackage(output, output_target_name)
-    for target in targets:
-        for arch in SUPPORTED_TARGETS[target]:
-            args = ["install", "--use-pep517", "--prefer-binary", "--force-reinstall", "--pre", "--no-cache-dir"] + packages
-            if requirement is not None:
-                with open(requirement, "r") as f:
-                    for pkg in f.readlines():
-                        if pkg != "":
-                            args.append(pkg.strip().replace("\n", ""))
-            if no_deps:
-                args.append("--no-deps")
-            args += ["--index-url", index_url]
-            args += ["--extra-index-url", "https://pypi.org/simple"]
-            call_pip(args, target, arch, package, include)
-            package.package_binaries(target, arch)
+    
+    # Build list of all packages with their customizations
+    all_package_specs = []
+    
+    # Parse command-line packages
+    for pkg_spec in packages:
+        pkg_name, pkg_targets = parse_package_customization(pkg_spec, list(targets))
+        all_package_specs.append((pkg_name, pkg_targets))
+    
+    # Parse requirement file packages
+    if requirement is not None:
+        with open(requirement, "r") as f:
+            for line in f.readlines():
+                line = line.strip().replace("\n", "")
+                if line and not line.startswith("#"):
+                    pkg_name, pkg_targets = parse_package_customization(line, list(targets))
+                    all_package_specs.append((pkg_name, pkg_targets))
+    
+    # Install each package for its specified targets
+    for pkg_spec, pkg_targets in all_package_specs:
+        for target in pkg_targets:
+            for arch in SUPPORTED_TARGETS[target]:
+                args = ["install", "--use-pep517", "--prefer-binary", "--force-reinstall", "--pre", "--no-cache-dir", pkg_spec]
+                if no_deps:
+                    args.append("--no-deps")
+                args += ["--index-url", index_url]
+                args += ["--extra-index-url", "https://pypi.org/simple"]
+                call_pip(args, target, arch, package, include)
+                package.package_binaries(target, arch)
+    
     package.make_xcode_frameworks(not no_scripts)
 
     for subdir, dirs, files in os.walk(package.bundle_path):
